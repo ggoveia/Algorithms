8fd7bb2d7de68241e3b0a84b55488b418d76a207
Giuliane Goveia <giuliane.ramos@gmail.com>
2018-02-03 22:01:23 -0200

    Adding Binary Search

---
+++ Algorithms/Types/Searching/BinarySearch.cs
@@ -0,0 +1,63 @@
+﻿namespace Algorithms.Types.Searching
+{
+    public class BinarySearch
+    {
+
+        private readonly int[] _sortedArray;
+        private readonly int _numberToBeFound;
+
+        public BinarySearch(int[] sortedArray, int numberToBeFound)
+        {
+            _sortedArray = sortedArray;
+            _numberToBeFound = numberToBeFound;
+        }
+
+
+        public bool Exists()
+        {
+            return LookForTheNumber();
+         }
+
+
+        private bool LookForTheNumber()
+        {
+                    
+            // -> Create start point;
+            var startPoint = 0;
+
+            // -> Create end point (consideringn that array starts at 0);
+            var endPoint = _sortedArray.Length -1 ;
+            
+
+            // 3 - Go to this part and start over again;
+            while (startPoint <= endPoint) {
+                    
+                // Define the middlepart of array;
+                int pointer = (endPoint - startPoint) / 2;
+
+                //Verify if the value has been found
+                if (_sortedArray[pointer] == _numberToBeFound)
+                {
+                    return true;                    
+                }
+
+                // Find the part what part does the value should be
+                if (_numberToBeFound < _sortedArray[pointer])
+                {
+                    // Define range for searching
+                    endPoint = pointer - 1;
+                }
+
+                if(_numberToBeFound > _sortedArray[pointer])
+                {
+                    // Define range for searching
+                    startPoint = pointer + 1;
+                    
+                }
+            }
+
+            //value has not been found
+            return false;
+        }
+    }
+}
---
+++ AlgorithmsTests/Types/Searching/BinarySearchTests.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms.Types.Searching;
+using NUnit.Framework;
+
+namespace AlgorithmsTests.Types.Searching
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        public void BinarySearch_CanFindAGivenNumberInASortedArray(int value)
+        {
+
+            var sortedArray = new[] {1,2,3,4,5,6,7,8,9,10};
+
+            var result = new BinarySearch(sortedArray, value).Exists();
+
+            Assert.AreEqual(true, result);
+
+        }
+
+        [TestCase(11)]
+        public void BinarySearch_CanNOTFindAGivenNumberInASortedArray(int value)
+        {
+
+            var sortedArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = new BinarySearch(sortedArray, value).Exists();
+
+            Assert.AreEqual(false, result);
+
+        }
+    }
+}
