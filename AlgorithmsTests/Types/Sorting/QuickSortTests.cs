7aa76873a022bd1f58508d1a4dd8d8c2597bcddc
Giuliane Goveia <giuliane.ramos@gmail.com>
2018-03-25 20:26:29 -0300

    Finishing quicksort

---
+++ AlgorithmsTests/Types/Sorting/QuickSortTests.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms;
+using NUnit.Framework;
+
+namespace AlgorithmsTests.Types.Searching
+{
+    [TestFixture]
+    public class QuickSortTests
+    {
+
+        [TestCase(new string []{  "31415926535897932384626433832795", "1", "3", "10", "3", "5" },
+            new string[] { "1", "3", "3", "5", "10", "31415926535897932384626433832795" })]
+        public void QuickSort_CanFindThePivot(string[] arr, string[] result) {
+
+            QuickSort Sorting = new QuickSort(arr);
+            var solution = Sorting.Solve();
+
+            Assert.AreEqual(result, solution);
+        }
+    }
+}
